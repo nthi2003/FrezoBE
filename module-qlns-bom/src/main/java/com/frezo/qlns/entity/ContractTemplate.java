a4b0c5c1279c390297834527b8449e78e85c2e86
thinvq <quocthi.dev@gmail.com>
2026-06-28 14:39:08 +0700

    1.1

--- module-qlns-bom/src/main/java/com/frezo/qlns/entity/ContractTemplate.java
+++ module-qlns-bom/src/main/java/com/frezo/qlns/entity/ContractTemplate.java
@@ -13,13 +13,15 @@ import lombok.*;
 @Entity
 @Table(name = "contract_templates")
 public class ContractTemplate extends BaseEntity {
+    @Column(name = "template_name", length = 500)
     private String name;
 
+    @Column(name = "contract_type", length = 100)
     private String type;
 
-    @Column(name = "file_url")
+    @Column(name = "file_url", columnDefinition = "TEXT")
     private String fileUrl;
 
-    @Column(name = "file_object_name")
+    @Column(name = "file_object_name", columnDefinition = "TEXT")
     private String fileObjectName;
 }
